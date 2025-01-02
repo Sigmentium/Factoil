aa2ef3a82aa3a7cc29aef9ff7c8e0c44bfdac204
Markusha <LegoAnimation2022@gmail.com>
2025-01-02 12:48:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-// const Main = document.getElementById('Main');
 
-// let DataMain;
 
 // document.getElementById('Click').addEventListener('click', function() {
-//     Main.innerHTML++;
-
-//     DataMain = localStorage.setItem('Main', Main);
 // });
-
-// window.onload = function() {
-//     localStorage.getItem('Main');
-// }
-
-const Main = document.getElementById('Main');
-
-let SaveData;
-
-window.onload = function() {
-    SaveData = localStorage.getItem('Main');
-    if (SaveData !== null) {
-        Main.innerHTML = SaveData;
-    }
-};
-
-document.getElementById('Click').addEventListener('click', function() {
-    Main.innerHTML++;
-    localStorage.setItem('Main', Main.innerHTML);
-});
-
-localStorage.clear();
